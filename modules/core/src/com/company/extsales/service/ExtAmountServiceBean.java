c62660ad0b8edcfc4595dc2e154c2d37c7108df6
Ivan I. Ovchinnikov <ocrio.box@gmail.com>
2018-02-14 18:30:00 +0400

    полнотекстовый

--- modules/core/src/com/company/extsales/service/ExtAmountServiceBean.java
+++ modules/core/src/com/company/extsales/service/ExtAmountServiceBean.java
@@ -1,7 +1,6 @@
 package com.company.extsales.service;
 
 import com.company.extsales.entity.ExtOrder;
-import com.company.sales.entity.Item;
 import com.company.sales.entity.Order;
 import com.company.sales.service.AmountServiceBean;
 
@@ -10,11 +9,8 @@ import java.math.BigDecimal;
 public class ExtAmountServiceBean extends AmountServiceBean {
     @Override
     public BigDecimal countAmount(Order order) {
+        BigDecimal result = super.countAmount(order);
         ExtOrder o = (ExtOrder) order;
-        BigDecimal result = BigDecimal.valueOf(0);
-        for (Item i : order.getItems()) {
-            result = result.add(i.getCost()).multiply(BigDecimal.valueOf((o.getIsUrgent() ? 1.25f : 1)));
-        }
-        return result;
+        return (o.getIsUrgent()) ? result.multiply(BigDecimal.valueOf(1.25)) :result;
     }
 }
